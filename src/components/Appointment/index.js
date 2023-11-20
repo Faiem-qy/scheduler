f316886d87fb98aeb40d4cb52898fe466659fa4f
Faiem-qy <f.quanyoung@gmail.com>
2023-11-20 00:19:29 -0500

    add ternary for show or empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,8 +6,17 @@ import Header from "components/Appointment/Header";
 
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
