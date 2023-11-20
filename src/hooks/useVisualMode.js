312046ccef4cddf667b986be0093825cb55e6e39
Faiem-qy <f.quanyoung@gmail.com>
2023-11-19 23:14:21 -0500

    implemented transition with Replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,9 +5,9 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); // track history
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
     setMode(newMode);
-    setHistory(prev => [...prev, newMode]);
+    setHistory(prev => (replace ? [...prev.slice(0,prev.length - 1), newMode] : [...prev, newMode]));
   }
 
   function back() {
