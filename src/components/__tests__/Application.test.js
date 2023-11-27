34062f50426c74e6a77d096fe98550c7f0b72101
Faiem-qy <f.quanyoung@gmail.com>
2023-11-26 21:04:22 -0500

    perform test coverage, added more tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, act } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("renders without crashing", async () => {
+  //If your component involves asynchronous operations (e.g., data fetching, useEffect with asynchronous code), make sure you're handling those within the act block.
+  await act(async () => {
+    render(<Application />);
+  });
 });
